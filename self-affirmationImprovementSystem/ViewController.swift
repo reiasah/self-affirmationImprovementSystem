5dfa1388354a23b15b2e1950eca6b05fbea476d6
rei asahina <33148369+reias219@users.noreply.github.com>
2022-07-17 11:50:03 +0900

    Initial Commit

---
+++ self-affirmationImprovementSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self-affirmationImprovementSystem
+//
+//  Created by rei asahina on 2022/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
